97fbaeaceebeb0c8b25220d514d8a77332d5e10a
kimbaoksqp123 <kimbaoksqp@gmail.com>
2023-05-29 12:06:19 +0700

    Create Data For User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 /**
@@ -15,16 +18,30 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+     protected $model = User::class;
+
     public function definition(): array
     {
+
+        $fake = \Faker\Factory::create('ja_JP');
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'username' => $fake->userName(),
+            'email' => $fake->email(),
+           
+            'password' => '12345678@', // password
+            'phoneNumber' => $fake->phoneNumber(),
+            'fullname' => $fake->name(),
+            'age' =>$fake->numberBetween(18,80),
+            'gender' => $fake->boolean(),
+            'userType' => '0', 
+
+
+            
         ];
     }
+
+   
 
     /**
      * Indicate that the model's email address should be unverified.
