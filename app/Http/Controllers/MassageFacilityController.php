c28afe7fdfe6bd273218c69e773fce869e3a08b9
tqhb2502 <tqhb2502@gmail.com>
2023-06-03 20:59:10 +0700

    modify_filter_by_input_and_add_filter_by_service_list

--- app/Http/Controllers/MassageFacilityController.php
+++ app/Http/Controllers/MassageFacilityController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MassageFacilityResource;
 use App\Models\MassageFacility;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Http\Request;
 
 class MassageFacilityController extends Controller
@@ -15,24 +17,47 @@ class MassageFacilityController extends Controller
 
     public function filter(Request $req) {
 
+        // get request data
         $input = $req->input;
-        $serviceList = $req->serviceList;
+        $serviceList = array($req->serviceList);
         $minPrice = $req->minPrice;
         $maxPrice = $req->maxPrice;
         $minRate = $req->minRate;
         $maxRate = $req->maxRate;
 
+        // instantiate query
         $query = MassageFacility::query();
-
+        
         // name, address
         if ($input) {
-            $query->where('name', 'like', "%{$input}%")
-                ->orWhere('location', 'like', "%{$input}%");
+
+            $inputWithSpaces = trim($input);
+            $inputWithoutSpaces = preg_replace('/\s+/', '', $input);
+            
+            $nameWithoutSpaces = "REPLACE(REPLACE(REPLACE(name, ' ', ''), '\t', ''), '\n', '')";
+            $locationWithoutSpaces = "REPLACE(REPLACE(REPLACE(location, ' ', ''), '\t', ''), '\n', '')";
+
+            $subQuery = MassageFacility::selectRaw("
+                id,
+                $nameWithoutSpaces as nameWithoutSpaces,
+                $locationWithoutSpaces as locationWithoutSpaces
+            ");
+
+            $query->joinSub($subQuery, 'sub_query', function (JoinClause $join) {
+                $join->on('massage_facilitys.id', '=', 'sub_query.id');
+            })
+                ->whereRaw("TRIM(name) LIKE ?", ['%' . $inputWithSpaces . '%'])
+                ->orWhereRaw("TRIM(location) LIKE ?", ['%' . $inputWithSpaces . '%'])
+                ->orWhereRaw("nameWithoutSpaces LIKE ?", ['%' . $inputWithoutSpaces . '%'])
+                ->orWhereRaw("locationWithoutSpaces LIKE ?", ['%' . $inputWithoutSpaces . '%']);
         }
 
         // massage service
-        if ($serviceList && count($serviceList) > 0) {
-            
+        if (count($serviceList) > 0 && !empty($serviceList[0]) ) {
+            $query->with('massage_services')
+                ->whereHas('massage_services', function (Builder $query) use ($serviceList) {
+                    $query->whereIn('serviceName', $serviceList);
+                });
         }
 
         // price
