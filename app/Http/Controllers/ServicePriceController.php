ff5780166d2b08d643d3a2bd4781c4f0495d3e2a
LosterJ <anhluong.tdnb@gmail.com>
2023-06-19 21:27:07 +0700

    Update MassageServiceController and ServicePriceController to add service information

--- app/Http/Controllers/ServicePriceController.php
+++ app/Http/Controllers/ServicePriceController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ServicePrice;
+use App\Models\MassageService;
 
 class ServicePriceController extends Controller
 {
-    public function store(Request $req) {
-        //todo
+    public function store(Request $req, MassageService $massageService) {
+        $servicePrice = ServicePrice::create([
+            'serviceID' => $massageService->id,
+            'price' => $req->price,
+            'durationTime' => $req->durationTime,
+        ]);
+
+        return response('ServicePriceControllerOk');
     }
 }
