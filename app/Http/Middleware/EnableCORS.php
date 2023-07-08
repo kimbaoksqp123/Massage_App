8ec13fa27337825ee44ccf9cbb4cb4e05d642552
tqhb2502 <tqhb2502@gmail.com>
2023-07-09 01:00:24 +0700

    add_enable_cors_middleware

---
+++ app/Http/Middleware/EnableCORS.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnableCORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'https://fe-codebase.vercel.app')
+            ->header('Access-Control-Allow-Methods', '*');
+    }
+}
