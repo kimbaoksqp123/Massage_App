035ba56a10001a46d470127597fcdc83ad5f1c7d
tqhb2502 <tqhb2502@gmail.com>
2023-07-10 15:24:52 +0700

    change_cors_config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://fe-codebase.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
